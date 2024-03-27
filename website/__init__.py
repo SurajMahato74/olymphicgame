b943351ebd2484c21886a9258543aa61c74f834b
suraj mahato <m.suraj1123@gmail.com>
2024-03-27 23:01:28 +0545

    init is restored

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import cv2
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
+    camera = cv2.VideoCapture(0)
 
     app.config['SECRET_KEY'] = 'Kathmandu@74'
     base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -25,7 +26,12 @@ def create_app():
         finally:
             print("db.create_all() in __init__.py was successful - no exceptions were raised")
 
-  
+    from .models import User, shows
+    from .views import views
+    from .auth import auth, init_auth
 
+    init_auth(app, camera)
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
 
     return app
